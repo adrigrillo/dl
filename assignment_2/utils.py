7664c4381d058c3e649ddd19b773bd15eaf084c7
Adrián Rodríguez Grillo <adrirgrillo@gmail.com>
2019-05-03 21:52:37 +0200

    Improved method that process the data to avoid the specification of the shape

--- assignment_2/utils.py
+++ assignment_2/utils.py
@@ -6,8 +6,7 @@ import numpy as np
 from sklearn.model_selection import KFold
 
 
-def process_data(time_series: np.ndarray, window_size: int, shape_x: Tuple = (3, 1),
-                 shape_y: Tuple = (1, 1)) -> Tuple[np.ndarray, np.ndarray]:
+def process_data(time_series: np.ndarray, window_size: int) -> Tuple[np.ndarray, np.ndarray]:
     """
     Method that process a time series and creates the data required to train and test the
     neural network.
@@ -18,12 +17,11 @@ def process_data(time_series: np.ndarray, window_size: int, shape_x: Tuple = (3,
 
     :param time_series: time series data
     :param window_size: number of data points that are included in the x elements
-    :param shape_x: shape of the elements of x. Default = (3, 1)
-    :param shape_y: shape of the elements of y. Default = (1, 1)
     :return: tuple with the values of x and y and the specified shapes
     """
-    # number of examples that can be created with the given time series
     n_possible_elements = time_series.shape[0] - window_size - 1
+    shape_x = (window_size, 1)
+    shape_y = (1, 1)
     x = np.empty((n_possible_elements,) + shape_x)
     y = np.empty((n_possible_elements,) + shape_y)
     for i in range(n_possible_elements):
@@ -32,11 +30,20 @@ def process_data(time_series: np.ndarray, window_size: int, shape_x: Tuple = (3,
         y_element = np.reshape(time_series[end], newshape=shape_y)
         x[i] = x_element
         y[i] = y_element
-    return np.array(x), np.array(y)
+    return x, y
 
 
 def generate_train_and_validation_sets(x_data: np.ndarray, y_data: np.ndarray, folds: int,
                                        seed: int = None) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+    """
+    Method that splits the data in training and test using k-folds.
+
+    :param x_data: inputs
+    :param y_data: value to predict outputs
+    :param folds: number of data divisions
+    :param seed: seed for the selection of the instances
+    :return: arrays with the train/test x and y instances
+    """
     k_folder = KFold(n_splits=folds, random_state=seed)
     x_train_folds = list()
     y_train_folds = list()
